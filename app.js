ec96da805898a3c7d74f09fe42f7cf7b639d44af
CarisAvi21 <shinnytinystar@hotmail.com>
2018-09-06 19:16:28 -0500

    Creando ruta absoluta

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
- const fs = require('fs');
+// hacer la ruta absolta 
+const path = require('path');
+const validateRout = (file) => {
+  if (path.isAbsolute(file)) {
+    console.log('Ruta absoluta');
+    console.log(path.isAbsolute());
+  } else {
+    console.log('Ruta relativa, se convierte en absoluta');
+    console.log(path.resolve());
+  }
+};
+validateRout('./README.md');
 
-const readReadme = (callBack) => {
-    fs.readFile('./README.md', 'utf8', (err, data)=>{
-        if (err){
-        console.log('Tienes un error');
+// funcion que lee el readme
+const fs = require('fs');
+const readReadme = (callback) => {
+  fs.readFile('./README.md', 'utf8', (err, data)=>{
+    if (err) {
+      console.log('Tienes un error');
     } else {
-       link(data);
+      link(data);
     }
+  });
+};  
+readReadme();
 
-    });
-    };  
-    readReadme();
+// función para encontrar loslinks en el readme 
+const link = (data) => {
+  console.log(data);
+  const regularExp = /(https?):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
+  const links = data.match(regularExp);
+  if (links) {
+    console.log(links);
+  } else {
+    console.log(error);
+  }
+};
 
-    const link = (data) => {
-        console.log(data)
-        let regularExp = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
-        let links = data.match(regularExp);
-        if (links) {
-            console.log(links)
-        } else {
-            console.log(error)
-        }
+// función para leer los links
 
-    };
 
+// función para validar los links
